8566f82530d8a1eb1fcd88cc9a035c73f0aad045
nrrao <nandanarao@yahoo.com>
2019-08-20 14:57:21 -0400

    delete completed

--- index.js
+++ index.js
@@ -1,11 +1,12 @@
 'use strict';
 
 const STORE = [
-  {name: "apples", checked: false},
-  {name: "oranges", checked: false},
-  {name: "milk", checked: true},
-  {name: "bread", checked: false}
+  {id: cuid(), name: "apples", checked: false},
+  {id: cuid(), name: "oranges", checked: false},
+  {id: cuid(), name: "milk", checked: true},
+  {id: cuid(), name: "bread", checked: false}
 ];
+
 
 function generateItemElement(item) {
   return `
@@ -21,6 +22,7 @@ function generateItemElement(item) {
       </div>
     </li>`;
 }
+
 
 function generateShoppingItemsString(shoppingList) {
   console.log("Generating shopping list element");
@@ -41,23 +43,65 @@ function renderShoppingList() {
 }
 
 
-function handleNewItemSubmit() {
-  // this function will be responsible for when users add a new shopping list item
-  console.log('`handleNewItemSubmit` ran');
+function addItemToShoppingList(itemName) {
+  console.log(`Adding "${itemName}" to shopping list`);
+  STORE.push({id: cuid(), name: itemName, checked: false});
 }
 
+function handleNewItemSubmit() {
+  $('#js-shopping-list-form').submit(function(event) {
+    event.preventDefault();
+    console.log('`handleNewItemSubmit` ran');
+    const newItemName = $('.js-shopping-list-entry').val();
+    $('.js-shopping-list-entry').val('');
+    addItemToShoppingList(newItemName);
+    renderShoppingList();
+  });
+}
+
+function toggleCheckedForListItem(itemId) {
+  console.log("Toggling checked property for item with id " + itemId);
+  const item = STORE.find(item => item.id === itemId);
+  item.checked = !item.checked;
+}
+
+
+function getItemIdFromElement(item) {
+  return $(item)
+    .closest('li')
+    .data('item-id');
+}
 
 function handleItemCheckClicked() {
-  // this function will be responsible for when users click the "check" button on
-  // a shopping list item.
-  console.log('`handleItemCheckClicked` ran');
+  $('.js-shopping-list').on('click', `.js-item-toggle`, event => {
+    console.log('`handleItemCheckClicked` ran');
+    const id = getItemIdFromElement(event.currentTarget);
+    toggleCheckedForListItem(id);
+    renderShoppingList();
+  });
 }
+/*var arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0];
 
-
+for( var i = 0; i < arr.length; i++){ 
+   if ( arr[i] === 5) {
+     arr.splice(i, 1); 
+   }
+}*/
+function deleteListItem(deleteId){
+  const item = STORE.find(item => item.id === deleteId);
+  console.log(item +"from deleteListItem");
+ STORE.splice(item,1);
+}
+/* */
 function handleDeleteItemClicked() {
-  // this function will be responsible for when users want to delete a shopping list
-  // item
-  console.log('`handleDeleteItemClicked` ran')
+  $('.js-shopping-list').on('click',`.js-item-delete`,event =>{
+    console.log('`handleDeleteItemClicked` ran');
+    const id = getItemIdFromElement(event.currentTarget);
+    console.log(id)
+    deleteListItem(id);
+    renderShoppingList();
+  });
+  
 }
 
 // this function will be our callback when the page loads. it's responsible for
@@ -69,7 +113,6 @@ function handleShoppingList() {
   handleNewItemSubmit();
   handleItemCheckClicked();
   handleDeleteItemClicked();
-
 }
 
 // when the page loads, call `handleShoppingList`
